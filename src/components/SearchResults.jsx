1eadac9c9c93956012ce2eaefab5bd125bf86a22
원도훈 <dnjsehgns98@gmail.com>
2024-11-09 16:05:34 +0900

    mission3

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -8,7 +8,7 @@ const SearchResults = ({ movies }) => {
   }
 
   return (
-    <div className="fixed top-16 left-0 w-screen h-screen z-20 bg-white">
+    <div className="fixed top-16 left-0 w-screen h-screen z-20 bg-white dark:bg-black">
       <div className="container mx-auto px-4 py-4">
         <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4">
           {movies.map((movie) => (
